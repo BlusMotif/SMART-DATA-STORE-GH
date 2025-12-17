a73383bb51db0cd5bdd475872e63555448fc8a7d
BlusMotif <blusmotif@gmail.com>
2025-12-17 01:35:28 +0000

    Temporarily hardcode Supabase config to test authentication

--- client/src/lib/supabaseClient.ts
+++ client/src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jddstfppigucldetsxws.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkZHN0ZnBwaWd1Y2xkZXRzeHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MTQ1MDUsImV4cCI6MjA4MTM5MDUwNX0.MwvCy45DluqdocUkkEhZ3QoGsQfYC30XGzbILT_JGgk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
